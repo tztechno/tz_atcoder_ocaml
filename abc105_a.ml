54b5dba7c0559dbcaa118a4d4f3d904ee6bfac4d
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-04-24 15:23:03 +0900

    Update abc105_a.ml

--- abc105_a.ml
+++ abc105_a.ml
@@ -5,6 +5,15 @@ abc105_a.ml
 #######################################
 #######################################
 #######################################
+let () =
+  let (n,k) = Scanf.sscanf (read_line ()) "%d %d" (fun n k -> (n, k)) in
+  let result =
+    if n mod k=0 then
+      "0"
+    else
+      "1"
+  in
+  print_endline result
 #######################################
 let f a b = if a mod b = 0 then 0 else 1;;
 let () = Scanf.scanf "%d %d" f
